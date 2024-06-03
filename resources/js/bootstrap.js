51e755bf7d473a21dfc08e999b0b4363933ab9ec
Aryanando PC Kantor <aryanando232@gmail.com>
2024-06-03 08:36:56 +0700

    Almost Fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,6 +35,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import DataTable from 'datatables.net-bs5';
 
-
-
 window.datatable = DataTable;
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
 use App\Http\Controllers\ttdController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
+
+// Auth::routes();
+Auth::routes(['register' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::group(['prefix' => 'v2'], function () {
+        Route::get('/', [ttdController::class, 'index'] )->name('v2-dashboard');
+        Route::post('/add', [ttdController::class, 'store'] );
+    });
+    
+});
 Route::group(['prefix' => 'v2'], function () {
-    Route::get('/', [ttdController::class, 'index'] )->name('v2-dashboard');
-    Route::post('/add', [ttdController::class, 'store'] );
     Route::get('/sprin/{id}', [ttdController::class, 'showV2'] );
 });
 
 Route::get('/sprin/{id}', [ttdController::class, 'show'] );
+
