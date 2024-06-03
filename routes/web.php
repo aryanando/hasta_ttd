034b0ee2d90b1ed839b00e9cbd738abf8da0a171
Aryanando PC Kantor <aryanando232@gmail.com>
2024-06-03 08:39:51 +0700

    Adding Auth

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 
 // Auth::routes();
-Auth::routes(['register' => true]);
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
